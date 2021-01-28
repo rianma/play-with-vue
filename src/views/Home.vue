6490ad4155339e80b9f31c690b1cf4f797b64cff
rianma <mmg9403@gmail.com>
2021-01-29 01:05:21 +0800

    feat(render): 演示 mounted() 后宏任务和微任务的执行顺序

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <span style="font-size: 100px">
+      {{ count }}
+    </span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+  count = 1
+
+  mounted() {
+    this.count = 2;
+
+    requestAnimationFrame(() => {
+      console.log('count=3');
+      this.count = 3;
+    });
+    Promise.resolve(null).then(() => {
+      // eslint-disable-next-line no-debugger
+      console.log('count=4');
+      this.count = 4;
+    });
+    setTimeout(() => {
+      console.log('count=5');
+      this.count = 5;
+    }, 0);
+  }
+}
 </script>
