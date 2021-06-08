ae27a25d3489f9d26ef4226bbebd4dc631c2c9e1
rianma <mmg9403@gmail.com>
2021-06-09 02:38:55 +0800

    feat: setup login with github with azure static web apps

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/azure">Azure</router-link> |
       <router-link to="/tasks-order">Tasks Order</router-link>
     </div>
     <router-view/>
---
+++ src/views/Azure.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="azure">
+    <a href="/.auth/login/github">Login</a>
+  </div>
+</template>
+<script lang="ts">
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-debugger */
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Firebase extends Vue {
+}
+</script>
+<style lang="scss" scoped>
+.avatar {
+  width: 100px;
+  height: 100px;
+}
+</style>
